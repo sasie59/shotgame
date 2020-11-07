05443e7ddf5abd2e48f9ce2df50688d8b0552081
sasie59 <sasie59@gmail.com>
2020-11-07 16:35:21 +0800

    transfer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import dog from "./dog.jpeg";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      life: 3,
+      score: 0,
+    };
+  }
+
+  handleClick = (e) => {
+    console.warn(e.target.name);
+    if (e.target.name ==='dog') {
+      this.setState({
+        score: this.state.score + 1
+      });
+    } else {
+      if(this.state.life === 1) {
+        alert('game over');
+        this.setState({
+          life: 3,
+          score: 0,
+        })
+      } else {
+        this.setState({
+          life: this.state.life - 1
+        });
+      }
+    } 
+  };
+
+
+  render() {
+    const { life, score } = this.state;
+    return (
+      <div>
+        <div className="center life">生命值：{life}</div>
+        <div className="outside">
+          <div className="frame" onClick={this.handleClick}>
+            <img
+              alt="狗"
+              src={dog}
+              name="dog"
+              className="dog"
+              style={{
+                animationDuration: `${4 / (score + 1)}s`,
+              }}
+            />
+          </div>
+        </div>
+        <div className="center score">得分：{score}</div>
+      </div>
+    );
+  }
 }
-
-export default App;
